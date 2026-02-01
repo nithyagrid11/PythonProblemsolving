64c116c0c050dfd31bf994647593dfee28f005de
nithyagrid11 <smalladi@griddynamics.com>
2026-02-01 12:04:43 +0530

    Update 003_longest_substring_without_repeating_characters.py

--- leetcode/003_longest_substring_without_repeating_characters.py
+++ leetcode/003_longest_substring_without_repeating_characters.py
@@ -1,3 +1,8 @@
+"""
+LeetCode 3: Longest Substring Without Repeating Characters
+Problem Link:
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+"""
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_len = 0
