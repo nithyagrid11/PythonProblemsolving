322402525b787f8fbf1972f86adbfb75b1cf8ead
nithyagrid11 <smalladi@griddynamics.com>
2026-02-01 11:59:24 +0530

    Create 003_longest_substring_without_repeating_characters.py

---
+++ Leetcode/leetcode/003_longest_substring_without_repeating_characters.py
@@ -0,0 +1,13 @@
+"""
+LeetCode 3: Longest Substring Without Repeating Characters
+Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+Pattern: Sliding Window
+
+Example:
+Input: s = "abcabcbb"
+Output: 3
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
