3a3b6b9115f81d909e7d4c3391d3184c65b21b6b
Nithya <smalladi@gmail.com>
2026-02-03 12:55:30 +0530

    practised on various iterable objects using lambda

--- PythonProblemsolving/lambdapractise.py
+++ PythonProblemsolving/lambdapractise.py
@@ -1,6 +1,6 @@
 #sum
-sum = lambda num : num +10
-print(sum(40))
+'''sum = lambda num : num +10
+print(sum(40))'''
 
 #is even
 even = lambda x : x % 2 == 0
@@ -55,3 +55,78 @@ result = list(
     )   
 )
 print(result)
+
+#Use sort() and lambda to sort employees by salary.
+employees = [
+    {"id": 1, "salary": 60000},
+    {"id": 2, "salary": 50000},
+    {"id": 3, "salary": 40000}
+]
+employees.sort(key = lambda x : x["salary"])
+emp = list(map(lambda x : x["id"],employees))
+print(emp)
+
+#Use map() and lambda to create a list of product names.
+products = [
+    {"name": "pen", "price": 10},
+    {"name": "book", "price": 50},
+    {"name": "bag", "price": 300}
+]
+print(list(map(lambda x : x["name"],products)))
+
+#Use filter() and lambda to keep only active users.
+users = [
+    {"username": "x", "active": True},
+    {"username": "y", "active": False},
+    {"username": "z", "active": True}
+]
+print(list(filter(lambda x : x["active"] == True, users)))
+
+#Use filter() and lambda to keep records with total score > 40.
+records = [
+    {"name": "A", "scores": [10, 20, 3]},
+    {"name": "B", "scores": [5, 15, 25]}
+]
+print(list(filter(lambda x : sum(x["scores"]) > 40,records)))
+
+#Find names of students whose average marks > 80.
+marks = {
+    "A": (78, 82, 90),
+    "B": (40, 55, 60),
+    "C": (88, 92, 95)
+}
+print(list(
+    map(
+        lambda x:x[0], filter(lambda x : sum(x[1]) / len(x[1]) > 80, marks.items())
+    )
+))
+
+#Create a new dictionary with final price after discount.
+#tuple(price,discount)
+prices = {
+    "pen": (10, 5),
+    "book": (50, 20),
+    "bag": (300, 50)
+}
+final_price = dict()
+# discount_amount = price * (discount/100)
+print(dict(map(lambda x : (
+    x[0],
+    x[1][0] - (x[1][0] * x[1][1] / 100)),prices.items()
+    )))
+
+#Use sorted() and lambda to sort items by the second value of the tuple.
+points = {
+    "A": (2, 3),
+    "B": (5, 1),
+    "C": (1, 4)
+}
+print(sorted(points.items(),key = lambda x : x[1][1]))
+
+#Use filter() and lambda to find months where offline sales > online sales.
+sales = {
+    "Jan": (100, 200),
+    "Feb": (150, 180),
+    "Mar": (90, 220)
+}
+print(list(filter(lambda x : x[1][0] < x[1][1],sales.items())))
