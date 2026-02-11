5b918cfc621b67da193260876e4e17435dc28c55
Nithya <smalladi@gmail.com>
2026-02-11 12:22:37 +0530

    resolved

--- leetcode/find_the_first_duplicate.py
+++ leetcode/find_the_first_duplicate.py
@@ -2,7 +2,7 @@ def first_duplicate(lst):
     seen = []
     for r in range(0,len(lst)):
         if lst[r] in seen:
-            return r
+            return lst[r]
         else:
-            seen.append(r)
-print(first_duplicate([1,2,3,1,2]))
+            seen.append(lst[r])
+print(first_duplicate([1,2,2,3,1,2]))
