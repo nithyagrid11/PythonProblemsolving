26010859431d4e8b7e867b3d419848223076a51b
Nithya <smalladi@gmail.com>
2026-02-26 09:58:35 +0530

    practising

--- DSA/single_linkedlist.py
+++ DSA/single_linkedlist.py
@@ -12,10 +12,10 @@ class SingleLinkedList:
         else:
             print('List: ')
             p = self.start
-            while p.next is not None:
+            while p.next is not None:#printing till the 2nd last node
                 print(p.value,end='->')
                 p = p.next
-            print(p.value)
+            print(p.value) #printing last value
     def insert_at_the_beginning(self,data):
         temp = Node(data)
         temp.next = self.start
@@ -26,7 +26,7 @@ class SingleLinkedList:
             self.start = temp
             return
         p = self.start #linkedlist is there 
-        while p.next is not None:#and traversing to the end
+        while p.next is not None:
             p = p.next
         p.next = temp #adding temp at the end
     def create_list(self):
@@ -36,6 +36,16 @@ class SingleLinkedList:
         for i in range(n):
             data = int(input('Enter the value: '))
             self.insert_at_end(data)
+    def insert_after(self,x,new_node):
+        p = self.start
+        while p is not None:
+            if p.value == x:
+                temp = Node(new_node)
+                temp.next = p.next
+                p.next = temp
+                return
+            p = p.next
+        print('Element not found')
 
 
 #menu
@@ -49,7 +59,8 @@ while True:
     #print('3: search for an element')
     print('4: Insert at the beginning')
     print('5: Insert at the end')
-    print('6:Exit')
+    print('6: Insert after a specific node')
+    print('7:Exit')
 
 
     choice = int(input('Choose one option: '))
@@ -62,6 +73,10 @@ while True:
         data = int(input('Enter the element to be inserted at the end: '))
         list1.insert_at_end(data)
     elif choice == 6:
+        x = int(input('Enter the element after which to insert: '))
+        new_node = int(input('Enter the new node to be inserted: '))
+        list1.insert_after(x,new_node)
+    elif choice == 7:
         break
     else:
         print('Wrong option')
