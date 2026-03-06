1ffc707e4b4e5b84a039f4f6d8ae20e1d6479904
Nithya <smalladi@gmail.com>
2026-03-06 11:45:53 +0530

    tried consequent 2nd largest sum and optimised it

--- DSA/second_largest_sum.py
+++ DSA/second_largest_sum.py
@@ -1,4 +1,4 @@
-def second_largest_sum():
+'''def second_largest_sum():
     list1 = list()
     n = int(input('Enter the range of list: '))
     for _ in range(n):# O(n)
@@ -11,4 +11,48 @@ def second_largest_sum():
     max_num = max(list1) #O(n)
     print(f'Second largest sum: {num_3 + max_num}')
 second_largest_sum()
-#O(n logn)
+#O(n logn)'''
+
+#consequent 2nd largest sum
+'''def consequent_sum():
+    numb_list = list()
+    n = int(input('Enter the range of list: '))
+    if n<3:
+        print("Need atleast 3 numbers")
+        return
+    for _ in range(n):
+        number = int(input('Enter the value: '))
+        numb_list.append(number)
+    print("List: ", numb_list)
+    sums = []
+    for i in range(n-1):
+        pair_sum = numb_list[i] + numb_list[i+1]
+        sums.append(pair_sum)
+    print("All consequent sums are: ",sums)
+    sums.sort()
+    print(f'2nd largest consequent sum is {sums[-2]}')
+consequent_sum()'''
+
+#optimised consequent 2nd largest
+def consequent_optimised():
+    numb_list = list()
+    n = int(input('Enter the range of list: '))
+    if n<3:
+        print("Need atleast 3 numbers")
+        return
+    for _ in range(n):
+        number = int(input('Enter the value: '))
+        numb_list.append(number)
+    print("List: ", numb_list)
+
+    largest = float('-inf')
+    second_largest = float('-inf')
+    for i in range(n-1):
+        pair_sum = numb_list[i] + numb_list[i+1]
+        if pair_sum > largest:
+            second_largest = largest
+            largest = pair_sum
+        elif pair_sum > second_largest and pair_sum != largest:
+            second_largest = pair_sum
+    print("2nd highest sum of consequent numbers is: ",second_largest)
+consequent_optimised()
