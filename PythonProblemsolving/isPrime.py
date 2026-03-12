ca001235977c6bf7b5522593d9c183ac40bfb02b
Nithya <smalladi@gmail.com>
2026-03-12 10:14:32 +0530

    new approach

--- PythonProblemsolving/isPrime.py
+++ PythonProblemsolving/isPrime.py
@@ -2,7 +2,7 @@ def isPrime(n):
     if n<=1:
         return 'Not Prime'
     for i in range(2,n):
-        if n==2 or n%i == 0:
+        if n%i == 0:
             return 'Not Prime'
     return 'Prime'
 print(isPrime(4))
@@ -10,3 +10,11 @@ print(isPrime(2))
 print(isPrime(5))
 print(isPrime(1))
 print(isPrime(-1))
+
+def isPrime(n):
+    if n<=1:
+        return 'Not Prime'
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return 'Not Prime'
+    return 'Prime'
