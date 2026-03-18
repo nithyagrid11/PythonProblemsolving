f92dcb272de210ce405a17b6ebf1ca25e63c3ea2
Nithya <smalladi@gmail.com>
2026-03-18 12:24:37 +0530

    added a response

--- log_analyzer/backend/main.py
+++ log_analyzer/backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from analyzer import analyze_logs
 
 app = FastAPI()
@@ -6,6 +6,9 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Smart Log Analyzer API is running"}
+@app.get("/favicon.ico")
+def favicon():
+    return Response(status_code = 204) #204 - no content, browser stays quiet
 @app.get("/analyze")
 def run_analysis():
     result = analyze_logs("server.log")
